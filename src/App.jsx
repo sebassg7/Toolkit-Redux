161b61c827afa54d27fc762394b92276edef8fe6
sebassg7 <sebastlopez@unicauca.edu.co>
2024-01-14 23:41:10 -0500

    decrement e incrementBy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { increment } from './store/slices/counter';
+import { decrement,increment, incrementBy } from './store/slices/counter';
+
 
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -7,7 +8,7 @@ import './App.css';
 function App() {
 
   const { counter } = useSelector( state=>state.counter );
-  const dispatch = useDispatch( increment );
+  const dispatch = useDispatch( increment,decrement,incrementBy );
 
   return (
     <>
@@ -19,10 +20,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a> */}
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is { counter }</h1>
       <div className="card">
         <button onClick={() => dispatch( increment() )}>
-          count is { counter }
+          Increment
+        </button>
+        <button onClick={() => dispatch( decrement() )}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch( incrementBy(2) )}>
+          Increment by 2
         </button>
       </div>
     </>
--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -6,15 +6,27 @@ initialState : {
     counter: 10,
 },
 reducers: {
-increment: (state) => {
+increment: ( state ) => {
     // Redux Toolkit allows us to write "mutating" logic in reducers. It
     // doesn't actually mutate the state because it uses the Immer library,
     // which detects changes to a "draft state" and produces a brand new
     // immutable state based off those changes
     state.counter += 1
-},},
+},
+decrement:( state ) => {
+    state.counter -= 1
+},
+
+incrementBy: ( state, action ) => {
+    state.counter = state.counter + action.payload
+}
+
+},
 });
 
 // Action creators are generated for each case reducer function
 export const { increment } = counterSlice.actions;
+export const { decrement } = counterSlice.actions;
+export const { incrementBy } = counterSlice.actions;
+
 
