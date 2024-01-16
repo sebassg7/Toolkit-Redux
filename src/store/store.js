daa091ffcbe5b933fa0e635bf928dc97d832de9b
sebassg7 <sebastlopez@unicauca.edu.co>
2024-01-16 16:42:07 -0500

    Consumir el API mediante el custom hook

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from './slices/counter';
 import { pokemonSlice } from './slices/pokemon';
+import { todosApi } from './apis';
+// import { getDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
         pokemons: pokemonSlice.reducer,
+        [todosApi.reducerPath]: todosApi.reducer, //Se pone entre corchetes porque es una propiedad computada /7 Alberga el resultado de los querys que disparemos
     },
+
+    middleware: ( getDefaultMiddleware ) => getDefaultMiddleware()
+    .concat( todosApi.middleware )
 })
