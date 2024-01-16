4c33e8fc767b02969e0d55a5e533de30cdb85895
sebassg7 <sebastlopez@unicauca.edu.co>
2024-01-16 12:23:34 -0500

    RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App.jsx';
-import { PokemonApp } from './PokemonApp.jsx';
+// import { PokemonApp } from './PokemonApp.jsx';
+import { TodoApp } from './TodoApp.jsx';
+
 
 import { store } from './store';
 import { Provider } from 'react-redux';
 import './index.css';
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PokemonApp />
+      <TodoApp />
     </Provider>,  
   </React.StrictMode>
 
